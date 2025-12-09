2acf71ad6b1950bfaabf8f87cb1fbdf3f4beafb7
MagicTeaMC <oneowo87@gmail.com>
2025-12-09 21:44:46 +0800

    website faster

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import playformCompress from '@playform/compress';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.milkteamc.org',
-  integrations: [sitemap()],
+  integrations: [sitemap(), playformCompress()],
 });
